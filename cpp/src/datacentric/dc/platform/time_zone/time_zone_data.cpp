be0230d52b801f21ff926599c81e7408cec1cf8e
Alexander Sokol <sokol@dualitygroup.com>
2019-08-18 13:44:22 -0400

    Added missing class time_zone to DataCentric.
    (cherry picked from commit aa2ddf6a384f77657cf1311b71a52f7e9d69c2cb)

---
+++ cpp/src/datacentric/dc/platform/time_zone/time_zone_data.cpp
@@ -0,0 +1,35 @@
+/*
+Copyright (C) 2013-present The DataCentric Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <dc/precompiled.hpp>
+#include <dc/implement.hpp>
+#include <dc/platform/time_zone/time_zone_data.hpp>
+#include <dc/platform/context/context_base.hpp>
+
+namespace dc
+{
+    dot::type_t time_zone_data_impl::type() { return typeof(); }
+    dot::type_t time_zone_data_impl::typeof()
+    {
+        static dot::type_t type_ =
+            dot::make_type_builder<self>("dc", "time_zone")
+            ->with_field("time_zone_id", &self::time_zone_id)
+            ->template with_base<record<time_zone_key_impl, time_zone_data_impl>>()
+            ->with_constructor(&make_time_zone_data, {  })
+            ->build();
+        return type_;
+    }
+}
---
+++ cpp/src/datacentric/dc/platform/time_zone/time_zone_data.hpp
@@ -0,0 +1,83 @@
+/*
+Copyright (C) 2013-present The DataCentric Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#pragma once
+
+#include <dc/declare.hpp>
+#include <dot/system/ptr.hpp>
+#include <dc/types/record/record.hpp>
+#include <dc/platform/time_zone/time_zone_key.hpp>
+
+namespace dc
+{
+    class time_zone_data_impl; using time_zone_data = dot::ptr<time_zone_data_impl>;
+    class time_zone_key_impl; using time_zone_key = dot::ptr<time_zone_key_impl>;
+
+    inline time_zone_data make_time_zone_data();
+
+    /// This interface provides timezone for the conversion between
+    /// datetime (by convention, always in UTC) and date, time,
+    /// and minute (by convention, always in a specific timezone).
+    ///
+    /// Only the following timezones can be defined:
+    ///
+    /// * UTC timezone; and
+    /// * IANA city timezones such as America/New_York
+    ///
+    /// Other 3-letter regional timezones such as EST or EDT are
+    /// not permitted because they do not handle the transition
+    /// between winter and summer time automatically for those
+    /// regions where winter time is defined.
+    ///
+    /// Because TimeZoneID is used to look up timezone conventions,
+    /// it must match either the string UTC or the code in IANA
+    /// timezone database precisely. The IANA city timezone code
+    /// has two slash-delimited tokens, the first referencing the
+    /// country and the other the city, for example America/New_York.
+    class DC_CLASS time_zone_data_impl : public record_impl<time_zone_key_impl, time_zone_data_impl>
+    {
+        typedef time_zone_data_impl self;
+        friend time_zone_data make_time_zone_data();
+
+    public: // FIELDS
+
+        /// Unique timezone identifier.
+        ///
+        /// Only the following timezones can be defined:
+        ///
+        /// * UTC timezone; and
+        /// * IANA city timezones such as America/New_York
+        ///
+        /// Other 3-letter regional timezones such as EST or EDT are
+        /// not permitted because they do not handle the transition
+        /// between winter and summer time automatically for those
+        /// regions where winter time is defined.
+        ///
+        /// Because TimeZoneID is used to look up timezone conventions,
+        /// it must match either the string UTC or the code in IANA
+        /// timezone database precisely. The IANA city timezone code
+        /// has two slash-delimited tokens, the first referencing the
+        /// country and the other the city, for example America/New_York.
+        dot::string time_zone_id;
+
+    public:
+        virtual dot::type_t type();
+        static dot::type_t typeof();
+    };
+
+    /// Create an empty instance.
+    inline time_zone_data make_time_zone_data() { return new time_zone_data_impl; }
+}
